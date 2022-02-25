d140809c969c2a483444bcf197ff4410da178f4e
Harsh Vadhiya <67258482+harshvadhiya@users.noreply.github.com>
2022-02-25 19:15:00 +0530

    Create Triangle with Angle (ANGTRICH).cpp

---
+++ Triangle with Angle (ANGTRICH).cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    if((a+b>c || b+c>a || a+c>b) && (a+b+c == 180) && a>0 && b>0 &&c>0) cout<<"YES";
+    else cout<<"NO";
+
+    return 0;
+}
